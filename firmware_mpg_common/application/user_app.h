1c83597728871b61fcfc137d2de55a9896d50114
Zhouxue1995 <1191125261@qq.com>
2016-06-08 11:34:03 +0800

    application ant slave 1

--- firmware_mpg_common/application/user_app.h
+++ firmware_mpg_common/application/user_app.h
@@ -23,7 +23,17 @@ Header file for yournewtaskname.c
 /**********************************************************************************************************************
 Type Definitions
 **********************************************************************************************************************/
-
+/* Required constants for ANT channel configuration */
+#define ANT_CHANNEL_USERAPP             (u8)0                 
+#define ANT_SERIAL_LO_USERAPP           (u8)0x03                 
+#define ANT_SERIAL_HI_USERAPP           (u8)0x26               
+#define ANT_DEVICE_TYPE_USERAPP         (u8)0                 
+#define ANT_TRANSMISSION_TYPE_USERAPP   (u8)0                 
+#define ANT_CHANNEL_PERIOD_LO_USERAPP   (u8)0x00              
+#define ANT_CHANNEL_PERIOD_HI_USERAPP   (u8)0x20              
+#define ANT_FREQUENCY_USERAPP           (u8)50                
+#define ANT_TX_POWER_USERAPP            RADIO_TX_POWER_0DBM
+#define TIMEOUT_VALUE           (u8)50
 
 /**********************************************************************************************************************
 Constants / Definitions
@@ -55,11 +65,11 @@ void UserAppRunActiveState(void);
 State Machine Declarations
 ***********************************************************************************************************************/
 static void UserAppSM_Idle(void);    
-
+static void UserAppSM_FailedInit(void);
 static void UserAppSM_Error(void);         
-static void UserAppSM_FailedInit(void);        
-
-
+static void UserAppSM_WaitChannelOpen(void);        
+static void UserAppSM_ChannelOpen(void);
+static void UserAppSM_WaitChannelClose(void);
 #endif /* __USER_APP_H */
 
 
